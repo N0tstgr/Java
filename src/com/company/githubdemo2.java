65a8b02e8e5d2202d2eb4f2d597fc2907633a0fb
N0tstgr <vikashkasaudhan321@gmail.comm>
2025-03-26 18:24:47 +0530

    4th commit

---
+++ src/com/company/githubdemo2.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class githubdemo2 {
+    public static void main(String[] args) {
+        int i = 7;
+        int a = 8;
+        int x = a + i;
+
+        System.out.println(x);
+    }
+}
