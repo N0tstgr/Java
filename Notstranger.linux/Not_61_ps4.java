c025d48188d563690d0829b84610fc60fc2b912b
N0tstgr <vikashkasaudhan321@gmail.com>
2025-08-09 01:15:32 +0530

    vid61

--- Notstranger.linux/Not_61_ps4.java
+++ Notstranger.linux/Not_61_ps4.java
@@ -11,11 +11,33 @@ class Library{
         no_of_books++;
         System.out.println(book+ "has been added!");
     }
-
+    void showAvailableBooks(){
+        System.out.println("Available BOoks are : ");
+        for (String book: this.books){
+            if(book == null){
+                break;
+            }
+            System.out.println("* "+ book);
+        }
+    }
+void issuebook(String book){
+    System.out.println("Books that can be issued ");
+        for (String b: this.books){
+            if(b == book){
+                System.out.println("The book has been issued ");
+            }
+            System.out.println("* "+ b);
+        }
+}
 }
 public class Not_61_ps4 {
     public static void main(String[] args) {
         Library central = new Library();
         central.addBooks("Tao Te Ching");
+        central.addBooks("Algorithm");
+        central.addBooks("Linux Terminal");
+        central.addBooks("how to start a startup");
+        central.issuebook("Linux Terminal");
+        central.showAvailableBooks();
     }
 }
