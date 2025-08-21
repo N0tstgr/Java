68eec838b67ab0bb7b71903de67c8562f3f10f95
N0tstgr <vikashkasaudhan321@gmail.com>
2025-08-21 23:50:40 +0530

    71

--- src/Luxxy/Not_vid71_runnableInterface.java
+++ src/Luxxy/Not_vid71_runnableInterface.java
@@ -1,20 +1,54 @@
 package Luxxy;
-class MythreadRun implements Runnable{
+class MyThreadRun implements Runnable{
     public void run(){
-        System.out.println("i am vikashtrillionaire not a thread1");
+        System.out.println("i am vikash trillionaire not a thread1");
+        System.out.println("i am vikash trillionaire not a thread1");
+        System.out.println("i am vikash trillionaire not a thread1");
+        System.out.println("i am vikash trillionaire not a thread1");
+        System.out.println("i am vikash trillionaire not a thread1");
+        System.out.println("i am vikash trillionaire not a thread1");
+        System.out.println("i am vikash trillionaire not a thread1");
+        System.out.println("i am vikash trillionaire not a thread1");
+        System.out.println("i am vikash trillionaire not a thread1");
+        System.out.println("i am vikash trillionaire not a thread1");
+        System.out.println("i am vikash trillionaire not a thread1");
+        System.out.println("i am vikash trillionaire not a thread1");
+        System.out.println("i am vikash trillionaire not a thread1");
+        System.out.println("i am vikash trillionaire not a thread1");
+        System.out.println("i am vikash trillionaire not a thread1");
     }
 }
 
-class MythreadRun2 implements Runnable{
+class MyThreadRun2 implements Runnable{
     public void run(){
-        System.out.println("i am vikashtrillionaire not a thread2");
+        System.out.println("i am vikash trillionaire not a thread2");
+        System.out.println("i am vikash trillionaire not a thread2");
+        System.out.println("i am vikash trillionaire not a thread2");
+        System.out.println("i am vikash trillionaire not a thread2");
+        System.out.println("i am vikash trillionaire not a thread2");
+        System.out.println("i am vikash trillionaire not a thread2");
+        System.out.println("i am vikash trillionaire not a thread2");
+        System.out.println("i am vikash trillionaire not a thread2");
+        System.out.println("i am vikash trillionaire not a thread2");
+        System.out.println("i am vikash trillionaire not a thread2");
+        System.out.println("i am vikash trillionaire not a thread2");
+        System.out.println("i am vikash trillionaire not a thread2");
+        System.out.println("i am vikash trillionaire not a thread2");
+        System.out.println("i am vikash trillionaire not a thread2");
+        System.out.println("i am vikash trillionaire not a thread2");
+        System.out.println("i am vikash trillionaire not a thread2");
     }
 }
 public class Not_vid71_runnableInterface {
     public static void main(String[] args) {
-        MythreadRun r1 = new MythreadRun();
-        MythreadRun r2 = new MythreadRun();
-        r1.start();
-        r2.start();
+
+        MyThreadRun bullet1 = new MyThreadRun();
+        Thread gun1 = new Thread(bullet1);
+
+        MyThreadRun2 bullet2 = new MyThreadRun2();
+        Thread gun2 = new Thread(bullet2);
+
+        gun1.start();
+        gun2.start();
     }
 }
