7ec6bbe2d8369ba7cc130eaa78f00794407bee0c
N0tstgr <vikashkasaudhan321@gmail.com>
2025-08-21 23:04:32 +0530

    thread

---
+++ src/Luxxy/Not_vid7_thread.java
@@ -0,0 +1,33 @@
+package Luxxy;
+
+class Mythread extends Thread{
+    @Override
+    public void run(){
+        int i =0;
+        while(i<1000) {
+            System.out.println("my  cooking restaurent thread is running");
+            System.out.println(" i am happy");
+            i++;
+        }
+    }
+}
+
+class Mythread2 extends Thread{
+    @Override
+    public void run(){
+        int i = 0;
+        while(i<1000) {
+            System.out.println("my thread2 is running for chatting with her ");
+            System.out.println(" i am great");
+            i++;
+        }
+    }
+}
+public class Not_vid7_thread {
+    public static void main(String[] args) {
+Mythread t1 = new Mythread();
+Mythread2 t2 = new Mythread2();
+t1.start();
+t2.start();
+    }
+}
