b912286fc5f5d518e36f2d5803d49125a49b1005
N0tstgr <vikashkasaudhan321@gmail.com>
2025-08-24 23:19:40 +0530

    81.1

--- src/Luxxy/Not_vid81_HandlingException.java
+++ src/Luxxy/Not_vid81_HandlingException.java
@@ -1,7 +1,14 @@
 package Luxxy;
 
+import java.util.Scanner;
+
 public class Not_vid81_HandlingException {
     public static void main(String[] args) {
-
+int [] marks = new int[5];
+marks[0] = 8;
+marks[1] = 5;
+marks[2] = 3;
+Scanner sc = new Scanner(System.in);
+j = sc.nextInt();
     }
 }
