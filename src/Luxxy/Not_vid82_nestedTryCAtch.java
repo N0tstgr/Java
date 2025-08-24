e544f85355190a254b2b40f44c62a29a195c1da6
N0tstgr <vikashkasaudhan321@gmail.com>
2025-08-24 23:49:23 +0530

    82.3

--- src/Luxxy/Not_vid82_nestedTryCAtch.java
+++ src/Luxxy/Not_vid82_nestedTryCAtch.java
@@ -3,7 +3,9 @@ package Luxxy;
 public class Not_vid82_nestedTryCAtch {
     public static void main(String[] args) {
 try {
-
+    System.out.println("welcome");
+}catch (Exception e){
+    System.out.println("Exception in level 1");
 }
     }
 }
