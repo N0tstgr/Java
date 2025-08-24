3f4cf1c4838eef941c23929266beff9ba83d3ddf
N0tstgr <vikashkasaudhan321@gmail.com>
2025-08-24 21:50:18 +0530

    78

---
+++ src/Luxxy/Not_vid78_errorsandexception.java
@@ -0,0 +1,8 @@
+package Luxxy;
+
+public class Not_vid78_errorsandexception {
+    public static void main(String[] args) {
+        int a  = 5;
+        System.out.println(a);
+    }
+}
---
+++ src/com/codewithNot/shape/Cylinder.java
@@ -0,0 +1,7 @@
+package com.codewithNot.shape;
+
+public class Cylinder {
+    public static void main(String[] args) {
+
+    }
+}
