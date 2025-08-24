7d6aa6d1682a61837671fbf4ec51111c2e84721b
N0tstgr <vikashkasaudhan321@gmail.com>
2025-08-24 22:33:57 +0530

    79

--- src/Luxxy/Not_vid78_errorsandexception.java
+++ src/Luxxy/Not_vid78_errorsandexception.java
@@ -4,6 +4,7 @@ public class Not_vid78_errorsandexception {
     public static void main(String[] args) {
         int a  = 5;
         int b = 8;
+
         System.out.println(a +b);
     }
 }
---
+++ src/Luxxy/Not_vid79_sy_Run_lgerror.java
@@ -0,0 +1,7 @@
+package Luxxy;
+
+public class Not_vid79_sy_Run_lgerror {
+    public static void main(String[] args) {
+
+    }
+}
