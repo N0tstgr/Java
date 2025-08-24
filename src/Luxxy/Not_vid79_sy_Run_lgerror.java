f73acf2347335eeeec8b5066336dd2dbd21d5237
N0tstgr <vikashkasaudhan321@gmail.com>
2025-08-24 22:38:55 +0530

    79.1

--- src/Luxxy/Not_vid79_sy_Run_lgerror.java
+++ src/Luxxy/Not_vid79_sy_Run_lgerror.java
@@ -2,6 +2,12 @@ package Luxxy;
 
 public class Not_vid79_sy_Run_lgerror {
     public static void main(String[] args) {
+        //logical errors demo
+//        write a program to print all prime numbers between 1 to 10
+        System.out.println(2);
+        for(int i=1; i<5; i++){
+            System.out.println(2*i+1);
+        }
 
     }
 }
