2b3fcace6862668573bd8eb3673675c17eb99870
N0tstgr <vikashkasaudhan321@gmail.com>
2025-08-24 22:42:57 +0530

    79.2

--- src/Luxxy/Not_vid79_sy_Run_lgerror.java
+++ src/Luxxy/Not_vid79_sy_Run_lgerror.java
@@ -2,12 +2,20 @@ package Luxxy;
 
 public class Not_vid79_sy_Run_lgerror {
     public static void main(String[] args) {
+        //Syntax Error demo
+//        int a = 0 Error no semicolon
+//        b = 9; Error no b is not declared
+
         //logical errors demo
 //        write a program to print all prime numbers between 1 to 10
         System.out.println(2);
         for(int i=1; i<5; i++){
             System.out.println(2*i+1);
         }
+
+
+        //Runtime Error
+
 
     }
 }
