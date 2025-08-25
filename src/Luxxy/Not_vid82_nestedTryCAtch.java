0f932a8a2dba4fd50e6f1df780a8e971e0ab79eb
N0tstgr <vikashkasaudhan321@gmail.com>
2025-08-25 22:57:55 +0530

    82.6

--- src/Luxxy/Not_vid82_nestedTryCAtch.java
+++ src/Luxxy/Not_vid82_nestedTryCAtch.java
@@ -1,15 +1,18 @@
 package Luxxy;
-
+import java.util.Scanner;
 public class Not_vid82_nestedTryCAtch {
     public static void main(String[] args) {
         int[] marks = new int[3];
         marks[0] = 9;
         marks[1] = 87;
         marks[2] = 76;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of index");
+        int m = sc.nextInt();
         try {
             System.out.println("Welcome to video on 82");
             try{
-                System.out.println(marks[9]);
+                System.out.println(marks[m]);
             }
             catch (ArrayIndexOutOfBoundsException e){
                 System.out.println("Sorry this index does not exist");
