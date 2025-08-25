8740515677a871473a985890d1e477f91059b512
N0tstgr <vikashkasaudhan321@gmail.com>
2025-08-25 22:48:08 +0530

    82.5

--- src/Luxxy/Not_vid82_nestedTryCAtch.java
+++ src/Luxxy/Not_vid82_nestedTryCAtch.java
@@ -6,15 +6,18 @@ public class Not_vid82_nestedTryCAtch {
         marks[0] = 9;
         marks[1] = 87;
         marks[2] = 76;
-
         try {
-            System.out.println("welcome to video no 82 ");
-            try {
+            System.out.println("Welcome to video on 82");
+            try{
                 System.out.println(marks[9]);
-            } catch (ArrayIndexOutOfBoundsException e) {
-                System.out.println("Exception in level 1");
             }
-
+            catch (ArrayIndexOutOfBoundsException e){
+                System.out.println("Sorry this index does not exist");
+                System.out.println("Exception in level 2");
+            }
+        }
+        catch (Exception e){
+            System.out.println("Exception in level 1");
         }
     }
 }
