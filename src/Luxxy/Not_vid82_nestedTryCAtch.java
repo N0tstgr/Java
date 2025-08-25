d657756afaf5bdff1f1eef589194e434fd8b2b19
N0tstgr <vikashkasaudhan321@gmail.com>
2025-08-25 21:48:49 +0530

    82.4

--- src/Luxxy/Not_vid82_nestedTryCAtch.java
+++ src/Luxxy/Not_vid82_nestedTryCAtch.java
@@ -2,17 +2,19 @@ package Luxxy;
 
 public class Not_vid82_nestedTryCAtch {
     public static void main(String[] args) {
-        int [] marks = new int[3];
+        int[] marks = new int[3];
         marks[0] = 9;
         marks[1] = 87;
         marks[2] = 76;
 
-try {
-    System.out.println("welcome to video no 82 ");
-}
+        try {
+            System.out.println("welcome to video no 82 ");
+            try {
+                System.out.println(marks[9]);
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println("Exception in level 1");
+            }
 
-catch (Exception e){
-    System.out.println("Exception in level 1");
-}
+        }
     }
 }
