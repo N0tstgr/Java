34f2fa86b04a0d548f4b9b4e17b381f3145c3ec0
N0tstgr <vikashkasaudhan321@gmail.com>
2025-08-27 23:55:18 +0530

    84

---
+++ src/Luxxy/Not_vid84_ThrowandThrows.java
@@ -0,0 +1,19 @@
+package Luxxy;
+
+public class Not_vid84_ThrowandThrows {
+
+    int divide(int a, int b) {
+
+
+        //made by vikash the trillionaire
+int result = a/b;
+return result;
+
+    }
+    public static void main(String[] args) {
+        int d = divide(6,2);
+        System.out.println(d);
+
+
+    }
+}
