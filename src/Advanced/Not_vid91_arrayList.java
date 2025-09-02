075ff952bdfa1b9bc4bda31619f3957bb9c44b06
N0tstgr <vikashkasaudhan321@gmail.com>
2025-09-02 22:27:17 +0530

    arraylist

--- src/Advanced/Not_vid91_arrayList.java
+++ src/Advanced/Not_vid91_arrayList.java
@@ -5,15 +5,24 @@ import java.lang.reflect.Array;
 public class Not_vid91_arrayList {
     public static void main(String[] args) {
         ArrayList<Integer>l1 = new ArrayList<>();
+        ArrayList<Integer>l2 = new ArrayList<>(6);
+
+        l2.add(78);
+        l2.add(38);
+        l2.add(98);
+
+
         l1.add(6);
         l1.add(7);
         l1.add(9);
-        l1.add(8);
-        l1.add(5);
+        l1.add(1,8);
+        l1.add(0,5);
+        l1.addAll(0,l2);
+        l1.clear();
+        System.out.println(l1.contains(90));
+//        for (int i =0; i<l1.size(); i++){
+//            System.out.println(l1.get(i));
 
-        for (int i =0; i<l1.size; i++){
-
-        }
 
     }
 }
