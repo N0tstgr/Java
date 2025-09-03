34d0c04414b7a6d278f4d5f2017aed2778eb5524
N0tstgr <vikashkasaudhan321@gmail.com>
2025-09-03 23:51:14 +0530

    linkedlist

--- src/Advanced/Not_vid92_Linkedlist.java
+++ src/Advanced/Not_vid92_Linkedlist.java
@@ -1,17 +1,18 @@
 package Advanced;
-import java.util.*;
-import java.lang.reflect.Array;
 
-public class Not_vid91_arrayList {
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class Not_vid92_Linkedlist {
     public static void main(String[] args) {
-        ArrayList<Integer>l1 = new ArrayList<>();
-        ArrayList<Integer>l2 = new ArrayList<>(6);
+        LinkedList<Integer> l1 = new LinkedList<>();
+        LinkedList<Integer>l2 = new LinkedList<>();
 
         l2.add(78);
         l2.add(38);
         l2.add(98);
 
-        l1.addLast(90);
+
         l1.add(6);
         l1.add(7);
         l1.add(9);
@@ -20,7 +21,8 @@ public class Not_vid91_arrayList {
         l1.add(0,5);
         l1.addAll(0,l2);
         l1.set(1,566);
-
+        l1.addLast(999);
+        l1.addFirst(909);
 
 //        l1.clear();
         System.out.println(l1.contains(90));
@@ -31,8 +33,6 @@ public class Not_vid91_arrayList {
         for (int i =0; i<l1.size(); i++) {
             System.out.print(l1.get(i));
             System.out.print(", ");
-
         }
-
-    }
+        }
 }
