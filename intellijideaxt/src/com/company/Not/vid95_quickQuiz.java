6fe2581a4602546100bb7cff966e282fe985fcb1
Your Name <vikashkasaudhan321@gmail.com>
2025-09-17 21:51:09 +0530

    MILLI-second

---
+++ intellijideaxt/src/com/company/Not/vid95_quickQuiz.java
@@ -0,0 +1,11 @@
+package Not;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class vid95_quickQuiz {
+    public static void main(String[] args) {
+       long millis =  System.currentTimeMillis();
+
+        System.out.println("this is the milli second " + millis);
+    }
+}
