3b9ecae72b9d158107ea3ca1cf07dc44c560f0f3
Your Name <vikashkasaudhan321@gmail.com>
2025-09-17 21:35:51 +0530

    dateAnd Time

--- intellijideaxt/src/com/company/Not/Not_vid95_DateAndTime.java
+++ intellijideaxt/src/com/company/Not/Not_vid95_DateAndTime.java
@@ -2,6 +2,8 @@ package Not;
 
 public class Not_vid95_DateAndTime {
     public static void main(String[] args) {
-
+        System.out.println(System.currentTimeMillis()/1000/3600/24/365);
+        System.out.println(System.currentTimeMillis()/1000/3600/24);
+        System.out.println(System.currentTimeMillis()/1000/3600);
     }
 }
