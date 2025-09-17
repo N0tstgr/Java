d6d1328db0795c8dccef63a2b923c3ddbf084e19
Your Name <vikashkasaudhan321@gmail.com>
2025-09-17 21:17:28 +0530

    HAAAAAAAAAAASH

--- intellijideaxt/src/com/company/Not/Not_vid94_hashset.java
+++ intellijideaxt/src/com/company/Not/Not_vid94_hashset.java
@@ -3,7 +3,12 @@ import java.util.HashSet;
 
 public class Not_vid94_hashset {
     public static void main(String[] args) {
-
-
+HashSet<Integer>myHashSet = new HashSet<>(9, 0.5f);
+myHashSet.add(5);
+        myHashSet.add(3);
+        myHashSet.add(2);
+        myHashSet.add(6);
+        myHashSet.add(8);
+        System.out.println(myHashSet);
     }
 }
