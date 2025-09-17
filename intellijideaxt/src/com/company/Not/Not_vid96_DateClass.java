9056a60c297b6deef4c6cfc8242d4097de8cd230
Your Name <vikashkasaudhan321@gmail.com>
2025-09-17 21:59:27 +0530

    DateClass

---
+++ intellijideaxt/src/com/company/Not/Not_vid96_DateClass.java
@@ -0,0 +1,12 @@
+package Not;
+import java.util.Date;
+public class Not_vid96_DateClass {
+    public static void main(String[] args) {
+//        System.out.println(Long.MAX_VALUE);
+//        System.out.println(System.currentTimeMillis());
+
+        Date d = new Date();
+        System.out.println(d);
+
+    }
+}
