8e5bae26aae33b86a24060a5d65b4ab78d4c8a11
Your Name <vikashkasaudhan321@gmail.com>
2025-09-18 21:12:06 +0530

    Calendar.2

--- intellijideaxt/src/com/company/Not/Not_vid98_calendar.java
+++ intellijideaxt/src/com/company/Not/Not_vid98_calendar.java
@@ -4,5 +4,6 @@ public class Not_vid98_calendar {
     public static void main(String[] args) {
 Calendar c = Calendar.getInstance();
         System.out.println(c.getCalendarType());
+        System.out.println(c.getCalendarType());
     }
 }
