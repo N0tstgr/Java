410bf54901b50093a3706302cc5fda7c43471df7
Your Name <vikashkasaudhan321@gmail.com>
2025-09-18 21:27:57 +0530

    TimeZone

--- intellijideaxt/src/com/company/Not/Not_vid98_calendar.java
+++ intellijideaxt/src/com/company/Not/Not_vid98_calendar.java
@@ -1,9 +1,15 @@
 package Not;
 import java.util.Calendar;
+import java.util.TimeZone;
 public class Not_vid98_calendar {
     public static void main(String[] args) {
-Calendar c = Calendar.getInstance();
+//Calendar c = Calendar.getInstance();
+//        System.out.println(c.getCalendarType());
+//        System.out.println(c.getTimeZone());
+
+
+        Calendar c = Calendar.getInstance(TimeZone.getTimeZone("Asia/Singapore"));
         System.out.println(c.getCalendarType());
-        System.out.println(c.getCalendarType());
+        System.out.println(c.getTimeZone().getID());
     }
 }
