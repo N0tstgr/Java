02f00b22d5746d250341398f948b24210c1065a8
Your Name <vikashkasaudhan321@gmail.com>
2025-09-18 21:53:21 +0530

    Gregorian2.0

--- intellijideaxt/src/com/company/Not/Not_vid99_GregorianCalendar.java
+++ intellijideaxt/src/com/company/Not/Not_vid99_GregorianCalendar.java
@@ -1,6 +1,7 @@
 package Not;
 import java.util.Calendar;
 import java.util.TimeZone;
+import java.util.GregorianCalendar;
 public class Not_vid99_GregorianCalendar {
     public static void main(String[] args) {
 //Calendar c = Calendar.getInstance();
@@ -8,10 +9,16 @@ public class Not_vid99_GregorianCalendar {
 //        System.out.println(c.getTimeZone());
 
 
-        Calendar c = Calendar.getInstance(TimeZone.getTimeZone("Asia/Singapore"));
-        System.out.println(c.getCalendarType());
-        System.out.println(c.getTimeZone().getID());
+        Calendar c = Calendar.getInstance();
+
         System.out.println(c.getTime());
+        System.out.println(c.get(Calendar.DATE));
+        System.out.println(c.get(Calendar.HOUR));
+        System.out.println(c.get(Calendar.MINUTE));
+        System.out.println(c.get(Calendar.SECOND));
+        System.out.println(c.get(Calendar.HOUR_OF_DAY) + ":" +  c.get(Calendar.MINUTE) + ":" + c.get(Calendar.SECOND));
+GregorianCalendar cal = new GregorianCalendar();
+        System.out.println(cal.isLeapYear(2025));
 
     }
 }
