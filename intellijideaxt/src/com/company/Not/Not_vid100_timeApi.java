d0e9b9fe299bf9a29667891d1acc8e6cb87fb5e3
Your Name <vikashkasaudhan321@gmail.com>
2025-09-20 19:57:20 +0530

    localDateTime

--- intellijideaxt/src/com/company/Not/Not_vid100_timeApi.java
+++ intellijideaxt/src/com/company/Not/Not_vid100_timeApi.java
@@ -2,12 +2,17 @@ package Not;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.time.LocalDateTime;
 
 public class Not_vid100_timeApi {
     public static void main(String[] args) {
 LocalDate d = LocalDate.now();
         System.out.println(d);
+
         LocalTime t = LocalTime.now();
         System.out.println(t);
+
+        LocalDateTime DT = LocalDateTime.now();
+        System.out.println(DT);
     }
 }
