25e1971af9c526c7b0d24f91f4dab8a862d6ac5c
Your Name <vikashkasaudhan321@gmail.com>
2025-09-20 21:31:42 +0530

    H:m a

--- intellijideaxt/src/com/company/Not/Not_vid101_DAteTimeFormatter.java
+++ intellijideaxt/src/com/company/Not/Not_vid101_DAteTimeFormatter.java
@@ -7,7 +7,7 @@ public class Not_vid101_DAteTimeFormatter {
         LocalDateTime dt = LocalDateTime.now();  //This is the DAte
         System.out.println(dt);
 
-        DateTimeFormatter df = DateTimeFormatter.ofPattern("dd/MM/yyyy --E"); //This is the format
+        DateTimeFormatter df = DateTimeFormatter.ofPattern("dd/MM/yyyy --E H:m a"); //This is the format
         DateTimeFormatter df2 = DateTimeFormatter.ISO_LOCAL_DATE;
 
         String myDate = dt.format(df); //Creating date String using date and formatter
