a96db27a2b33404ed91c0803aa28f0eadac07b63
Your Name <vikashkasaudhan321@gmail.com>
2025-09-20 21:02:43 +0530

    101-localdatetime

---
+++ intellijideaxt/src/com/company/Not/Not_vid101_DAteTimeFormatter.java
@@ -0,0 +1,10 @@
+package Not;
+
+import java.time.LocalDateTime;
+
+public class Not_vid101_DAteTimeFormatter {
+    public static void main(String[] args) {
+        LocalDateTime dt = LocalDateTime.now();
+        System.out.println(dt);
+    }
+}
